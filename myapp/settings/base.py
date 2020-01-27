4a8c901611e8788a3cfa3a6b740890462fca25a5
Ubuntu <ubuntu@ip-172-31-36-150.ap-northeast-2.compute.internal>
2020-01-27 02:00:40 +0000

    settings-rest

--- myapp/settings/base.py
+++ myapp/settings/base.py
@@ -39,8 +39,17 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'myapp.home',
-    'myapp.item'
+    'myapp.item',
+    'rest_framework',
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        # 'rest_framework.permissions.IsAdminUser',
+    ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 10
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
